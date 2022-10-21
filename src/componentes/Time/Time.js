d21a2c952f385b8df2eb11a9688bbc29cc3b8f9c
Gomes <joaovitor.jgn@gmail.com>
2022-10-20 21:01:39 -0300

    Adicionado Card dos times

--- src/componentes/Time/Time.js
+++ src/componentes/Time/Time.js
@@ -1,9 +1,11 @@
 import './time.css'
 
 function Time (props) {
+    const CSS = {backgroundColor: props.corSecundaria}
+    const cssBorder = {borderColor: props.corPrimaria}
     return (
-        <section className='time'>
-            <h3>{props.nome}</h3>
+        <section className='time' style={CSS}>
+            <h3 style={cssBorder}>{props.nome}</h3>
         </section>
     )
 }
